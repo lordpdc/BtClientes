b525661fc80ea8866435ef07321b924f50bbf09c
GabrielGDB <gabo.user01@gmail.com>
2016-10-30 19:26:54 -0600

    Mis clases
    es lo que tengo funcional, con lo de enviar no probado

---
+++ blotproy/BToothConnectThread.java
@@ -0,0 +1,108 @@
+package com.example.gd.blotproy;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Environment;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.UUID;
+
+public class BToothConnectThread {
+
+    private final BluetoothSocket socket;
+    private final BluetoothDevice deviceSrv;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private InputStream in;
+    private OutputStream out;
+    private Activity act;
+
+    private static final UUID MY_UUID = UUID.fromString("04c6093b-0000-1000-8000-00805f9b34fb");
+
+    public BToothConnectThread(Activity act, BluetoothDevice server){
+        BluetoothSocket tmp =  null;
+        deviceSrv = server;
+        this.act = act;
+        try{
+            tmp = deviceSrv.createRfcommSocketToServiceRecord(MY_UUID);
+
+        }catch(IOException exp){
+
+        }
+        socket = tmp;
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+    public void connectDevice(){
+        mBluetoothAdapter.cancelDiscovery();
+        try{
+            socket.connect();
+            out = socket.getOutputStream();
+            in = socket.getInputStream();
+        }catch(IOException connectException){
+            try{
+                socket.close();
+            }catch(IOException exp){}
+            return;
+        }
+
+    }
+
+    public void pgDown(){
+        try {
+            out.write(1);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void pgUp(){
+        try {
+            out.write(2);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void send() {
+        File file = new File(
+                Environment.getExternalStorageDirectory(),
+                "test.txt");
+
+        byte[] bytes = new byte[1024];
+
+            BufferedInputStream bis;
+            try {
+                bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytes, 0, bytes.length);
+                OutputStream os = socket.getOutputStream();
+                os.write(bytes, 0, bytes.length);
+                os.flush();
+                socket.close();
+/*
+            in = socket.getInputStream();
+            out = new FileOutputStream(file);
+            BufferedOutputStream bos = new BufferedOutputStream(out);
+            OutputStreamWriter osw = new OutputStreamWriter(out);
+            int bytesRead = in.read(bytes, 0, bytes.length);
+            out.write(file);
+            out.close();
+            socket.close();*/
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
---
+++ blotproy/ListDispActivity.java
@@ -0,0 +1,137 @@
+package com.example.gd.blotproy;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListDispActivity extends AppCompatActivity {
+
+    // Return Intent extra
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    //Codigos para los intents
+    private static final int REQUEST_ENABLE_BT = 2;
+    //manejador del bluetooth
+    private BluetoothAdapter mBluetoothAdapter;
+    private ArrayAdapter<String> listDispAdapter;
+    ListDispActivity thisAct;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_disp);
+
+        setResult(Activity.RESULT_CANCELED);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, filter);
+
+        listDispAdapter = new ArrayAdapter<String>(this,R.layout.nombre_dispositivo);
+        ListView listaDispositivos = (ListView) findViewById(R.id.list_disp_emparejados);
+        listaDispositivos.setAdapter(listDispAdapter);
+        listaDispositivos.setOnItemClickListener(mDispositivosClickListener);
+
+        //Adicionar cdigo para que la lista reaccione el click del usuario
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Toast.makeText(this, "Bluetooth is not running", Toast.LENGTH_SHORT).show();
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+        thisAct = this;
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Make sure we're not doing discovery anymore
+        // Unregister broadcast listeners
+        this.unregisterReceiver(mReceiver);
+    }
+
+    // The on-click listener for all devices in the ListViews
+    private AdapterView.OnItemClickListener mDispositivosClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+            // Cancel discovery because it's costly and we're about to connect
+            mBluetoothAdapter.cancelDiscovery();
+            // Get the device MAC address, which is the last 17 chars in the View
+            String info = ((TextView)v).getText().toString();
+            String address = info.substring(info.length() - 17);
+            // Create the result Intent and include the MAC address
+            Intent intent = new Intent();
+            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            // Set result and finish this Activity
+            setResult(Activity.RESULT_OK, intent);
+            finish();
+        }
+    };
+
+
+    public void buscarBtnClick(View v){
+
+        buscarDisp();
+    }
+
+    private void buscarDisp(){
+        setProgressBarIndeterminateVisibility(true);
+        setTitle(R.string.titulo_buscando_disp);
+        if (mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+        mBluetoothAdapter.startDiscovery();
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // If it's already paired, skip it, because it's been listed already
+                listDispAdapter.add(device.getName() + "|" + device.getAddress());
+                // When discovery is finished, change the Activity title
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                setProgressBarIndeterminateVisibility(false);
+                setTitle(R.string.titulo_app);
+                if (listDispAdapter.getCount() == 0) {
+                    String noDevices = getResources().getText(R.string.no_encontrado).toString();
+                    listDispAdapter.add(noDevices);
+                }
+            }
+        }
+    };
+}
